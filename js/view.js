5d641ba22f89a2bdc99f807a55847924ac9f743d
NolanDuthil <144225548+NolanDuthil@users.noreply.github.com>
2023-12-14 11:57:02 +0100

    it-10

--- js/view.js
+++ js/view.js
@@ -18,6 +18,37 @@ V.uicalendar = new Calendar('#calendar', {
     taskView: false,
     eventView: ['time'],
   },
+  theme: {
+    week: {
+      dayName: {
+        borderLeft: 'none',
+        borderTop: '1px solid #B213F2',
+        borderBottom: '1px solid #B213F2',
+        backgroundColor: '#CBB7F4',
+        date:'#A713F2',
+      },
+      dayGrid: {
+        backgroundColor: 'rgba(81, 92, 230, 0.05)',
+      },
+      dayGridLeft: {
+        borderRight: 'none',
+        backgroundColor: 'rgba(81, 92, 230, 0.05)',
+        width: '144px',
+      },
+      timeGridLeftAdditionalTimezone: {
+        backgroundColor: '#e5e5e5',
+      },
+      timeGridHalfHourLine: {
+        borderBottom: '1px solid #B213F2',
+      },
+      nowIndicatorPast: {
+        border: '1px solid #B213F2',
+      },
+      futureTime: {
+        color: '#A713F2',
+      },
+    }
+  },
   month: {
     startDayOfWeek: 1,
     dayNames: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
@@ -29,9 +60,7 @@ V.uicalendar = new Calendar('#calendar', {
     time: function (event) {
       return `<span style="color: white;">${event.title}</span>`;
     }
-  },
-
-
+  }
 });
 
 let nav = document.querySelector('.nav');
@@ -40,9 +69,9 @@ let search = document.querySelector("#search");
 search.addEventListener("keyup", handlerClickOnNav)
 
 V.colorMap = {
-  mmi1 : {CM :"#660000", TD:"#990000",TP:"#CC0000",AUTRE:"#FF0000"},
-  mmi2 : {CM :"#355E3B", TD:"#228B22", TP:"#50C878",AUTRE:"#00FF00"},
-  mmi3 : {CM:"#00008B",TD:"#0047AB",TP:"#000FFF",AUTRE:"#0000FF"}
+  mmi1: { CM: "#660000", TD: "#990000", TP: "#CC0000", AUTRE: "#FF0000" },
+  mmi2: { CM: "#355E3B", TD: "#228B22", TP: "#50C878", AUTRE: "#00FF00" },
+  mmi3: { CM: "#00008B", TD: "#0047AB", TP: "#000FFF", AUTRE: "#0000FF" }
 }
 
 export { V };
